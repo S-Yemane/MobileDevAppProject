6a76e6aaae6c66015b3e8272aee35ef4e1d73a02
Shalom Yemane <yemaneshalom@gmail.com>
2020-03-24 21:39:43 -0700

    Initial Commit

---
+++ MazeAppProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MazeAppProject
+//
+//  Created by CSUFTitan on 3/24/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
