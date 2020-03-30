e44a8ba80f50fbe045828b6cd0fb10d94615182e
Shalom Yemane <yemaneshalom@gmail.com>
2020-03-29 21:37:58 -0700

    First commit

---
+++ MazeAppProject/MazeCell.swift
@@ -0,0 +1,9 @@
+//
+//  MazeCell.swift
+//  MazeAppProject
+//
+//  Created by CSUFTitan on 3/24/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import Foundation
---
+++ MazeAppProject/MazeView.swift
@@ -0,0 +1,9 @@
+//
+//  MazeView.swift
+//  MazeAppProject
+//
+//  Created by CSUFTitan on 3/29/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import Foundation
